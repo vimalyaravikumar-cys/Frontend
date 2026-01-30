c92c88af6a0f0e7d8ffe31da2d16e135d9ebea8d
vimalyaravikumar-cys <vimalyaravikumar@gmail.com>
2026-01-30 11:29:19 +0530

    cc

--- demo.py
+++ demo.py
@@ -2,4 +2,10 @@ import streamlit as st
 st.title("Election-2026!!")
 st.subheader("Voter details:")
 name = st.text_input("Enter your name:")
-dob = st.number_input("Enter your DOB:")
+age = st.number_input("Enter your Age:")
+if st.button("Enter"):
+    if age>= 18:
+        st.text("Eligible to vote!")
+    else:
+        st.text(st.error("Not eligible to vote"))
+st.divider()            
